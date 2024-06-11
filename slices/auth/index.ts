29c9de7a9ca4ce72369f7e4997e3e33c83912511
aman.nagal <aman.nagal@propvivo.com>
2024-06-11 12:57:02 +0530

    changes in code

--- slices/auth/index.ts
+++ slices/auth/index.ts
@@ -21,56 +21,7 @@ export const authApi = apiSlice.injectEndpoints({
       }),
       invalidatesTags: ["login"],
     }),
-    // login: builder.mutation<AuthUser, { email: string; password: string }>({
-    //   query: (body) => ({
-    //     method: "POST",
-    //     url: "/authenticate",
-    //     body,
-    //   }),
-    //   invalidatesTags: ["login"],
-    // }),
   }),
 });
 
 export const  { useLoginMutation} = authApi
-
-// import { BaseQueryApi } from "@reduxjs/toolkit/dist/query/baseQueryTypes";
-// import { MaybePromise } from "@reduxjs/toolkit/dist/query/tsHelpers";
-// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-// import { RootState } from "../../store";
-// import { User } from "../../store/type";
-
-// type Prepare = {
-//   prepareHeaders?: (
-//     headers: Headers,
-//     api: Pick<BaseQueryApi, "getState" | "endpoint" | "type" | "forced">
-//   ) => MaybePromise<Headers>;
-// };
-
-// const prepareHeaders: Prepare["prepareHeaders"] = (headers, { getState }) => {
-//   const token = (getState() as RootState)?.auth?.api_token;
-//   if (token) {
-//     headers.set("accept", `application/json`);
-//     headers.set("authorization", `Bearer ${token}`);
-//   }
-//   return headers;
-// };
-
-// export const api = createApi({
-//   reducerPath: "api",
-//   baseQuery: fetchBaseQuery({
-//     baseUrl: process.env.REACT_APP_API_URL,
-//     prepareHeaders,
-//   }),
-//   endpoints: (builder) => ({
-//     login: builder.mutation<User, { email: string; password: string }>({
-//       query: (body) => ({
-//         method: "POST",
-//         url: "/api/authAdmin",
-//         body,
-//       }),
-//     }),
-//   }),
-// });
-
-//export const { useLoginMutation } = api;
